93370c04ee630d6505f17698057e6286da8ceb6f
BjornL1 <>
2023-10-20 10:00:33 +0000

    Add function for adding critical level and planned sales data

--- run.py
+++ run.py
@@ -38,17 +38,60 @@ def display_header(worksheet):
     print("This code always runs.")
 
 
+def get_planned_sales_data():
+    """
+    Get planned sales figures input from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 5 numbers separated
+    by commas. The loop will repeatedly request data until it is valid.
+    """
+    while True:
+        print("Please enter planned sales for next 4 weeks/months.")
+        print("Data should be five numbers, separated by commas.")
+        print("Example: 1000,200,30,400,50\n")
+
+        data_str = input("Enter your data here:\n")
+
+        planned_sales_data = data_str.split(",")
+
+        if validate_data(planned_sales_data):
+            print("Planned data is updated!")
+            break
+
+    return planned_sales_data
+
+
+def critical_level():
+    """
+    Get a critical level input from the user.
+    Run a while loop to collect a valid integer value from the user
+    via the terminal, which must be between 1 and 50.
+    """
+    while True:
+        print("Please enter the critical level (a number between 1 and 50):")
+
+        try:
+            critical_value = int(input("Enter your critical level here: "))
+            if 1 <= critical_value <= 50:
+                print("Critical level entered by the user:", critical_value)
+                return critical_value
+            else:
+                print("Critical level must be 1 and 50. Please try again.")
+        except ValueError:
+            print("Invalid inpu, whole number between 1 and 50.")
+
+
 def get_sales_data():
     """
     Get sales figures input from the user.
     Run a while loop to collect a valid string of data from the user
-    via the terminal, which must be a string of 6 numbers separated
+    via the terminal, which must be a string of 5 numbers separated
     by commas. The loop will repeatedly request data until it is valid.
     """
     while True:
         print("Please enter sales data from the last market.")
         print("Data should be six numbers, separated by commas.")
-        print("Example: 10,20,30,40,50,60\n")
+        print("Example: 10,20,30,40,50\n")
 
         data_str = input("Enter your data here:\n")
 
@@ -65,7 +108,7 @@ def validate_data(values):
     """
     Inside the try, convert all string values into integers.
     Raise a ValueError if strings cannot be converted into int
-    or if there aren't exactly 6 values.
+    or if there aren't exactly 5 values.
     """
     try:
         [int(value) for value in values]
@@ -147,6 +190,8 @@ def main():
     Run all program functions
     """
     display_header(SHEET.worksheet("sales"))
+    planned_sales_data = get_planned_sales_data()
+    critical = critical_level()
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_worksheet(sales_data, "sales")
